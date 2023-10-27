fd1ba931a315b9b230d876163fcffde42f861cd9
GiorgioBolzoni <gbolzoni96@gmail.com>
2023-10-27 15:55:18 +0200

    inserita immagine

--- js/script.js
+++ js/script.js
@@ -3,7 +3,11 @@ const { createApp } = Vue;
 const miaApp = createApp({
     data(){
         return{
-            title: '<h1>Hello Vue!</h1>'
+            title: '<h1>Hello Vue!</h1>',
+            immagine: "https://c4.wallpaperflare.com/wallpaper/1020/1/213/world-of-warcraft-battle-for-azeroth-video-games-warcraft-alliance-wallpaper-thumb.jpg"
         }
     }
 }).mount('#app');
+
+
+
